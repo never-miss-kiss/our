b69e033b234c1eda6a216b8a8b862197e407d7d1
never-miss-kiss <931721403@qq.com>
2018-06-04 20:49:47 +0800

    tian  对跑腿的dao层的相关接口进行了添加，对跑腿service层的接口和Helpmapper进行了修改

--- HamiSchool/src/main/java/com/gem/hami/service/HelpService.java
+++ HamiSchool/src/main/java/com/gem/hami/service/HelpService.java
@@ -9,8 +9,8 @@ public interface HelpService {
 //    /help/findHelpByCondition.action
 //    显示所有跑腿信息（按学校)
 //   按用户号查询
-//   按发布时间，热度,点击量排序
-// userId 用户号，schoolId 学校号，sortId（排序方式：1.按发布时间，2.热度,3.点击量排序）
+//   按发布时间,点击量排序
+// userId 用户号，schoolId 学校号，sortId（排序方式：1.按发布时间，2点击量排序）
     public List<HelpInfo> findHelpByCondition(int userId,int schoolId,int sortId);
 
 //    /help/addHelp.action
@@ -35,7 +35,6 @@ public interface HelpService {
     public boolean addCommentReply(HelpCommentReply helpCommentReply);
 
 
-
     //     /help/removeHelp.action
 //            删除跑腿信息
 //type 跑腿类型 ,helpId跑腿主键id
@@ -43,12 +42,10 @@ public interface HelpService {
 
 //    /help/removeHelpComment.action
 //            删除跑腿信息评论
-    public boolean removeHelpComment(HelpComment helpComment);
+    public boolean removeHelpComment(int helpCommentId);
 
 //    /help/removeHelpCommentReply.action
 //            删除跑腿信息评论回复
-    public boolean removeHelpCommentReply(HelpCommentReply helpCommentReply);
-
-
+    public boolean removeHelpCommentReply(int replyId);
 
 }
